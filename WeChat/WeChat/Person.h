7035891d335b4268a7fc06e5a349974ef21476fc
陈方坤 <1191317659@qq.com>
2016-08-31 22:45:08 +0800

    添加Person类

---
+++ WeChat/WeChat/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  WeChat
+//
+//  Created by chenfangkun on 16/8/31.
+//  Copyright © 2016年 chenfangkun. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
